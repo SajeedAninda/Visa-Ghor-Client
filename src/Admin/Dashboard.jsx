d90b53fdcd759cce0d03f9b1cb356d5454f65301
SajeedAninda <sajaninda@gmail.com>
2023-12-07 13:58:07 +0600

    Made dashboard page Respnsive

--- src/Admin/Dashboard.jsx
+++ src/Admin/Dashboard.jsx
@@ -5,7 +5,7 @@ import Lottie from 'lottie-react';
 const Dashboard = () => {
     return (
         <div className='w-[90%] mx-auto'>
-            <div className='flex justify-center items-center'>
+            <div className='flex flex-col md:flex-row my-12 md:my-6 lg:my-2 justify-center items-center'>
                 <div className='flex-1'>
                     <h1 className='text-7xl md:text-8xl lg:text-9xl font-bold'><span className='text-[#952895]'>Admin</span> <br /> <span>Dashboard</span> <br /></h1>
                     <h2 className='text-[#0b65b2] text-5xl font-bold'>Control Your Site from here</h2>
