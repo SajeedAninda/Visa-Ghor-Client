91cef1f67bba7fe57edca0846cb0460418f3d27c
SajeedAninda <sajaninda@gmail.com>
2024-01-28 14:52:22 +0600

    Address Change

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
                   </div>
 
                   <div className="text-center font-semibold text-[#033430] mt-2 text-lg pt-2 flex items-center justify-center gap-2">
-                    <span>Baitul Khair Tower, 48/A-B <br />Level-11/1107, Purana Paltan, Dhaka</span>
+                    <span>292 Shatabdi Center, F-17/2nd Floor <br />Inner Circular Road, Fakirapool, Motijheel, Dhaka</span>
                   </div>
                 </div>
               </div>
